aebe1a0004a32a8ce3005046f79d28ae130983e7
江立 <322137@qq.com>
2016-09-20 22:38:51 +0800

    Initial Commit

---
+++ RevealAllInOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RevealAllInOne
+//
+//  Created by Aimy on 9/20/16.
+//  Copyright © 2016 Aimy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
