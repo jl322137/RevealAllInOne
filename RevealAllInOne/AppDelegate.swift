e4c46b7ab0606a2d5c235ebb7409c86d16a5ed00
江立 <322137@qq.com>
2016-09-20 22:51:27 +0800

    update

--- RevealAllInOne/AppDelegate.swift
+++ RevealAllInOne/AppDelegate.swift
@@ -16,6 +16,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        #if DEBUG
+            let reveal = getenv("Reveal")
+            if strcmp(reveal, "1") == 0 {
+                if let path = Bundle.main.path(forResource: "Frameworks/libReveal", ofType: "dylib") {
+                    dlopen(path, RTLD_NOW | RTLD_NODELETE)
+                }
+            }
+
+            if strcmp(reveal, "2") == 0 {
+                if let path = Bundle.main.path(forResource: "Frameworks/RevealServer", ofType: "framework"), let bundle = Bundle(path: path) {
+                    do {
+                        try bundle.loadAndReturnError()
+                    }
+                    catch _ { }
+                }
+            }
+
+        #endif
+
         return true
     }
 
